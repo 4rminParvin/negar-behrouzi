a0a29985f504e0dc5aa159d048104324b35d183c
4rminParvin <armin.p4rvin@gmail.com>
2022-02-01 13:47:11 +0330

    clarify

--- JS/index.js
+++ JS/index.js
@@ -1,2 +1,15 @@
+// Update copyright date
 let date = new Date();
 document.getElementById("year").innerHTML = date.getFullYear();
+
+// Auto-resizable mail textarea
+const textarea = document.getElementsByTagName("textarea");
+for (let i = 0; i < textarea.length; i++) {
+  textarea[i].setAttribute("style", "height:" + (textarea[i].scrollHeight) + "px;overflow-y:hidden;");
+  textarea[i].addEventListener("input", OnInput, false);
+}
+
+function OnInput() {  
+  this.style.height = "auto";
+  this.style.height = (this.scrollHeight) + "px";
+}
